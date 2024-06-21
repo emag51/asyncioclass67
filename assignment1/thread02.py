f02ddba32f98a1ed94cd8f31d289a7b137e3fb04
emag51 <61967786+emag51@users.noreply.github.com>
2024-06-21 10:12:22 +0700

    finish thread02

--- assignment1/thread02.py
+++ assignment1/thread02.py
@@ -1,1 +1,14 @@
-# running a function with arguments in another thread
+# running a function in another thread
+from time import sleep, ctime
+from threading import Thread
+
+def task(sleep_time, message):
+    sleep(sleep_time)
+    print(f'{ctime()} {message}')
+
+thread = Thread(target=task, args=(1.5, 'New message from another thread'))
+
+thread.start()
+
+print(f'{ctime()} Waiting for the thread...')
+thread.join()
