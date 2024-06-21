91cae8e3e344f6eb16ff62843b13867d21300ae9
emag51 <61967786+emag51@users.noreply.github.com>
2024-06-21 10:22:22 +0700

    finish thread01, 02, 03 with comment

--- assignment1/thread01.py
+++ assignment1/thread01.py
@@ -2,13 +2,17 @@
 from time import sleep, ctime
 from threading import Thread
 
+# a custom function that blocks for the moment
 def task():
+    # block for a moment
     sleep(1)
+    # display a message
     print(f'{ctime()} This is from another thread')
 
+# create a thread
 thread = Thread(target=task)
-
+# run the thread
 thread.start()
-
+# waait for the thread to finish
 print(f'{ctime()} Waiting for the thread...')
 thread.join()
--- assignment1/thread03.py
+++ assignment1/thread03.py
@@ -1,1 +1,19 @@
 # extending the Thread class
+from time import sleep, ctime
+from threading import Thread
+
+#custom thread class
+class CustomThread(Thread):
+    def run(self):
+        # block for a moment
+        sleep(1)
+        # display a message
+        print(f'{ctime()} This is coming from another thread')
+
+# create the thread
+thread = CustomThread()
+# start the thread
+thread.start()
+# wwait for the thread to finish
+print(f'{ctime()} Waiting for the thread to finish')
+thread.join()
